2cea42c87c61d1f42db3f48edaed0e5efae4bbb9
Malvin King <malvintech2@gmail.com>
2025-11-24 12:51:20 +0200

    Create recipes.js

---
+++ src/data/recipes.js
@@ -0,0 +1,47 @@
+export const recipes = [
+  {
+    id: 1,
+    name: 'Broiler Starter Feed',
+    costSavings: '40%',
+    image: 'https://images.unsplash.com/photo-1550989460-0adf9ea622e2?w=500&h=500&fit=crop',
+    ingredients: [
+      'Maize (50kg)',
+      'Soya (20kg)',
+      'Fishmeal (10kg)',
+      'Vitamins (2kg)',
+      'Salt (1kg)'
+    ],
+    instructions: 'Mix all ingredients thoroughly using a feed mixer. Ensure even distribution of vitamins and minerals. Feed to broiler chicks for the first 4 weeks. Store in a cool, dry place.',
+    ageGroup: '0-4 weeks'
+  },
+  {
+    id: 2,
+    name: 'Layer Mash Recipe',
+    costSavings: '35%',
+    image: 'https://images.unsplash.com/photo-1589923188900-85dae523342b?w=500&h=500&fit=crop',
+    ingredients: [
+      'Maize (45kg)',
+      'Soya (18kg)',
+      'Limestone (12kg)',
+      'Fishmeal (8kg)',
+      'Premix (2kg)'
+    ],
+    instructions: 'Grind maize and soya to fine powder. Mix all ingredients thoroughly. The limestone provides calcium for strong eggshells. Ideal for egg-laying hens from 18 weeks onwards.',
+    ageGroup: '18+ weeks'
+  },
+  {
+    id: 3,
+    name: 'Grower Feed Formula',
+    costSavings: '38%',
+    image: 'https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=500&h=500&fit=crop',
+    ingredients: [
+      'Maize (48kg)',
+      'Soya (22kg)',
+      'Wheat bran (15kg)',
+      'Fishmeal (5kg)',
+      'Vitamins (2kg)'
+    ],
+    instructions: 'Crush maize to medium size particles. Mix all ingredients well using a feed mixer. This balanced formula supports healthy growth. Feed from 5-18 weeks for optimal development.',
+    ageGroup: '5-18 weeks'
+  }
+];
